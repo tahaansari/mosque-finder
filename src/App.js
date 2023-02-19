f18350aa1733e9343450f6de248bc9fe715ce16b
Taha Ansari <t.ansari@clerkswell.com>
2023-02-19 12:40:03 +0530

    wip

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  if ("geolocation" in navigator) {
+    console.log("Available");
+    navigator.geolocation.getCurrentPosition(function (position) {
+      console.log("Latitude is :", position.coords.latitude);
+      console.log("Longitude is :", position.coords.longitude);
+    });
+  } else {
+    console.log("Not Available");
+  }
+  return (
+    <div className="App">
+      <h1>Mosque Finder</h1>
+    </div>
+  );
 }
 
 export default App;
